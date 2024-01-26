b16a1360fab945be1213e91ac04f3f88cdab158a
AColderMilk <kerwinjunyi.tan@gmail.com>
2024-01-27 02:18:10 +0800

    Create deploy.py
    Script to deploy the machine learning model on Gradio.

---
+++ deploy.py
@@ -0,0 +1,24 @@
+import gradio as gr
+import tensorflow as tf
+import cv2
+import numpy as np
+
+model = tf.keras.models.load_model(r'C:\Users\ForTh\Downloads\Comp Projects\Concrete Crack Detection\code\saved_model\resnet50v2_model2')
+
+
+def detect_crack(img):
+    img = cv2.resize(img, (227, 227))  # Assuming this size from your provided code
+    img = img / 255.0  # Normalizing as per code
+    img = np.expand_dims(img, axis=0)  # Expanding dimensions to match the model input shape
+
+    prediction = model.predict(img)
+
+    if prediction > 0.5:
+        return "Crack detected"
+    else:
+        return "No crack detected"
+
+
+iface = gr.Interface(fn=detect_crack, inputs="image", outputs="text")
+
+iface.launch()
